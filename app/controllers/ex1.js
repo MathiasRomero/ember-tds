5b354773ad18b0d25a1c6ac8de797ba41a9592b5
Mathias Romero <77613856+MathiasRomero@users.noreply.github.com>
2021-01-26 16:57:27 +0100

    Exercice 1 complet

---
+++ app/controllers/ex1.js
@@ -0,0 +1,48 @@
+import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {tracked} from "@glimmer/tracking";
+
+export default class Ex1Controller extends Controller {
+  @tracked content = '';
+  @tracked info ='';
+
+  MAX=100;
+
+  get style(){
+    if(this.size > 50){
+      return 'info';
+    }
+    if(this.size > 20){
+      return 'warning';
+    }
+    return 'danger';
+
+  }
+
+
+  get size(){
+    return this.MAX - this.content.length;
+  }
+
+  @action save(){
+    console.log('Test save '+this.content);
+    if(this.info !=''){
+      this.info = "Note enregistrée";
+    }
+
+  }
+
+  @action supprimer(){
+    this.content = '';
+  }
+
+  @action update(){
+    if(this.content !=''){
+      this.info = "Note modifiée";
+    }
+  }
+
+  @action clear(){
+    this.info ='';
+  }
+}
