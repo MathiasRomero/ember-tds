dc5c57bc2a0ca9682c55e2a73139e1aabfe75641
Romero Mathias <Matatore2001@gmail.com>
2021-02-08 10:40:23 +0100

    debut ajout code promo

--- app/classes/services.js
+++ app/classes/services.js
@@ -1,10 +1,44 @@
+
 export default class Services {
   services = [];
-  total = 0;
+  promo = {
+    B22: 0.05,
+    AZ: 0.01,
+    UBOAT: 0.02,
+  };
+
+  get promoValue() {
+
+    let code = document.getElementById("promo").value;
+    let check = document.getElementById('promoCheck');
+
+    if (check.checked) {
+      //document.getElementById('remise').style.display = 'block';
+      //document.getElementById('total').visible;
+      if (code == 'B22') {
+        return this.sumActive * this.promo.B22;
+      }
+      if (code == 'AZ') {
+        return this.sumActive * this.promo.AZ;
+      }
+      if (code == 'UBOAT') {
+        return this.sumActive * this.promo.UBOAT;
+      }
+    } else {
+      //document.getElementById('remise').style.display = 'none';
+      //document.getElementById('total').style.display = 'none';
+    }
+    return 0;
+  }
+
+  get priceWithPromo() {
+    return this.sumActive - this.promoValue;
+  }
 
   constructor(serv) {
     this.services = serv;
   }
+
 
   get countActive() {
     console.log(
