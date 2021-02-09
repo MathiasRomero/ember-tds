1c32bd8fa0876e4696c6ab4294251073b0b254c6
Romero Mathias <Matatore2001@gmail.com>
2021-02-09 16:26:28 +0100

    Liste des produits disponibles

---
+++ app/classes/Produit.js
@@ -0,0 +1,9 @@
+export default class Produit{
+
+  produitDispo = [];
+
+
+  constructor(prod) {
+    this.produitDispo = prod;
+  }
+}
--- app/routes/step1.js
+++ app/routes/step1.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { datas } from 'tds/data/datas';
+import Produit from '../classes/Produit';
 
-export default class Step1Route extends Route {}
+export default class Step1Route extends Route {
+  model() {
+    return new Produit(datas);
+  }
+}
