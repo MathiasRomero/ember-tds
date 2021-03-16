2864a9ed3f2bbe69b200d0ff9f512b31a650d37b
Mathias Romero <77613856+MathiasRomero@users.noreply.github.com>
2021-03-16 18:55:08 +0100

    Connexion fonctionnelle

--- app/models/employee.js
+++ app/models/employee.js
@@ -1,5 +1,10 @@
-import Model from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class EmployeeModel extends Model {
+
+  @attr('String')name;
+  @attr('String')password;
+  @attr('String')email;
+
 
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,40 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+
+  @service userAuth;
+
+  model(){
+    return {};
+  }
+
+@action
+ login(name,password){
+
+    console.log("test connexion");
+    this.store
+      .query('employee', {
+        filter: {
+          name: name,
+        },
+      })
+      .then((users) => {
+        if (users.length) {
+          let user = users.firstObject;
+          if (password == user.password) {
+            this.userAuth.login(user);
+            console.log("User connected");
+            //this.transition;
+          }else{
+            console.log("Bad Password");
+          }
+        }
+      });
+  }
+
 }
+
+
+
